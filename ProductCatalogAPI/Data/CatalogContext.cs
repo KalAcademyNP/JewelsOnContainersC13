4fd3f5592e51f1687297ef19f3b88f64c68fbdff
Kal <info@kalacademy.org>
2022-07-17 12:23:27 -0700

    EFCore instructions

---
+++ ProductCatalogAPI/Data/CatalogContext.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductCatalogAPI.Domain;
+
+namespace ProductCatalogAPI.Data
+{
+    public class CatalogContext : DbContext
+    {
+        public CatalogContext(DbContextOptions options) : base(options)
+        { }
+
+        public DbSet<CatalogType> CatalogTypes { get; set; }
+        public DbSet<CatalogBrand> CatalogBrands { get; set; }
+        public DbSet<CatalogItem> Catalog { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CatalogType>(e =>
+            {
+                e.Property(t => t.Id)
+                    .IsRequired()
+                    .ValueGeneratedOnAdd();
+
+                e.Property(t => t.Type)
+                    .IsRequired()
+                    .HasMaxLength(100);
+            });
+
+            modelBuilder.Entity<CatalogBrand>(e =>
+            {
+                e.Property(t => t.Id)
+                    .IsRequired()
+                    .ValueGeneratedOnAdd();
+
+                e.Property(t => t.Brand)
+                    .IsRequired()
+                    .HasMaxLength(100);
+            });
+
+            modelBuilder.Entity<CatalogItem>(e =>
+            {
+                e.Property(t => t.Id)
+                    .IsRequired()
+                    .ValueGeneratedOnAdd();
+
+                e.Property(t => t.Name)
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+                e.Property(t => t.Price)
+                    .IsRequired();
+
+                e.HasOne(t => t.CatalogBrand)
+                    .WithMany()
+                    .HasForeignKey(t => t.CatalogBrandId);
+
+                e.HasOne(t => t.CatalogType)
+                    .WithMany()
+                    .HasForeignKey(t => t.CatalogTypeId);
+            });
+        }
+
+    }
+}
--- ProductCatalogAPI/Startup.cs
+++ ProductCatalogAPI/Startup.cs
@@ -2,10 +2,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using ProductCatalogAPI.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,6 +28,8 @@ namespace ProductCatalogAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<CatalogContext>(options =>
+                options.UseSqlServer(Configuration["ConnectionString"]));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
