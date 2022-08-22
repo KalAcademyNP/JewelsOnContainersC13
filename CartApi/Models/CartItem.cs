e644510fc3210a398506f4b32e05efd652bd6327
Kal <info@kalacademy.org>
2022-08-22 12:49:00 -0700

    Fix bad request issue

--- CartApi/Models/CartItem.cs
+++ CartApi/Models/CartItem.cs
@@ -2,7 +2,7 @@
 {
     public class CartItem
     {
-        public int Id { get; set; }
+        public string Id { get; set; }
         public string ProductId { get; set; }
         public string ProductName { get; set; }
         public string PictureUrl { get; set; }
--- WebMvc/Infrastructure/CustomHttpClient.cs
+++ WebMvc/Infrastructure/CustomHttpClient.cs
@@ -64,6 +64,8 @@ namespace WebMvc.Infrastructure
             var requestMessage = new HttpRequestMessage(method, uri);
             requestMessage.Content = new StringContent(JsonConvert.SerializeObject(item),
                                         System.Text.Encoding.UTF8, "application/json");
+            //requestMessage.Content = new StringContent("{\"buyerId\":\"me@myemail.com\",\"items\": [{\"id\": 1,\"productId\": \"7\",\"productname\": \"Blue Star 2\",\"pictureurl\": \"http://externalcatalogbaseurltobereplaced/api/pic/7\",\"unitprice\": 110,\"quantity\": 4,\"oldunitprice\": 0}]}",
+            //                            System.Text.Encoding.UTF8, "application/json");
 
             if (authorizationToken != null)
             {
